e92e915dfb0836026d4ffa5fcb9034dcdf949d78
Ashe Urban <ashe@goldentongue.com>
2024-06-10 11:15:49 -0700

    End of video 2!
    Animal collision set up

--- Code/Animal/Animal.gd
+++ Code/Animal/Animal.gd
@@ -6,68 +6,66 @@ class_name Animal
 
 var current_position: Vector3
 var target_position: Vector3
-var lerp_percentage: float = 1.0 # 0-100% | 1 / 100
+var position_lerp: float = 1.0 # 0-100% | 1 / 100
+
+var current_rotation: float 
+var target_rotation: float
+var rotation_lerp: float
 
 func _ready():
 	current_position = position
 	target_position = position
+	area_entered.connect(OnAreaEntered)
 	
 func _process(delta):
-	
-	if lerp_percentage >= 1.0:
-		if Input.is_action_just_pressed("move_left"):
-			target_position = current_position + Vector3.LEFT
-			lerp_percentage = 0.0
-			crab.rotation_degrees.y = 90
-		if Input.is_action_just_pressed("move_right"):
-			target_position = current_position + Vector3.RIGHT
-			lerp_percentage = 0.0
-			crab.rotation_degrees.y = -90
-		if Input.is_action_just_pressed("move_forward"):
-			target_position = current_position + Vector3.FORWARD # this is equal to Vector3.(0,0,-1)
-			lerp_percentage = 0.0
-			crab.rotation_degrees.y = 0
-		if Input.is_action_just_pressed("move_backward"):
-			target_position = current_position + Vector3.BACK
-			lerp_percentage = 0.0
-			crab.rotation_degrees.y = 180
-
-	if lerp_percentage < 1.0:
-		lerp_percentage += delta / stride_speed
-		lerp_percentage = clamp(lerp_percentage, 0.0, 1.0)
-		position = lerp(current_position, target_position, lerp_percentage)
+	if position_lerp < 1.0:
+		position_lerp += delta / stride_speed
+		rotation_lerp += delta / stride_speed
+		
+		position_lerp = clamp(position_lerp, 0.0, 1.0)
+		rotation_lerp = clamp(rotation_lerp, 0.0, 1.0)
+		
+		position = lerp(current_position, target_position, position_lerp)
+		crab.rotation_degrees.y = lerp(current_rotation, target_rotation, rotation_lerp)
 	else: 
 		current_position = target_position
+		current_rotation = target_rotation
+		
+		if Input.is_action_just_pressed("move_left"):
+			target_position = current_position + Vector3.LEFT
+			position_lerp = 0.0
+			target_rotation = 90.0
+			rotation_lerp = 0.0
+			
+		if Input.is_action_just_pressed("move_right"):
+			target_position = current_position + Vector3.RIGHT
+			position_lerp = 0.0
+			target_rotation = -90.0
+			rotation_lerp = 0.0
+			
+		if Input.is_action_just_pressed("move_forward"):
+			target_position = current_position + Vector3.FORWARD # this is equal to Vector3.(0,0,-1)
+			position_lerp = 0.0
+			target_rotation = 0.0
+			rotation_lerp = 0.0
+			
+		if Input.is_action_just_pressed("move_backward"):
+			target_position = current_position + Vector3.BACK
+			position_lerp = 0.0
+			target_rotation = 180.0
+			rotation_lerp = 0.0
+			#if current_rotation < 1.0:
 
-
-
-
-
-#const SPEED = 5.0
-#const JUMP_VELOCITY = 4.5
-#
-## Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-#
-#
-#func _physics_process(delta):
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity.y -= gravity * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	#var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	#if direction:
-		#velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
-#
-	#move_and_slide()
+func OnAreaEntered(area: Area3D):
+	if area is Road:
+		print("You made it across the road!", area)
+	if area is Vehicle:
+		print("XX/ You were hit by a vehicle!", area)
+	if area is River:	
+		print("Swallowed by the depths!", area)
+	if area is Vessel:
+		print("Riding the: ", area)
+	if area is Roost:
+		print("We made it safely to our roost!", area)	
+		
+	
